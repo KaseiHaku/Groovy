0b6a00fe8eb5c3b7a0d8075b42e86918fdf83210
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2020-01-08 20:32:31 +0800

    Update syntax.groovy

--- syntax.groovy
+++ syntax.groovy
@@ -1,93 +1,126 @@
 #!/usr/bin/env groovy
-// 单行注释
-/* 多行注释 */
-/** 文档注释 */
 
-// as 关键字用于取别名
-import java.sql.Date as SQLDate   
 
+import groovy.lang.Closure
+import java.sql.Date as SQLDate         // as 关键字用于取别名
+
+
+
+/******************************************************** Variable ****************************************************/
+/** 变量赋值 */
+def var0 = 'str'
+def (int var1, String var2, var3) = [1, 'q', false]
 
 /******************************************************** String ****************************************************/
 /** String Summary
-StringType              StringSyntax            Interpolated            Multiline           Escape character
-Single-quoted           '…'                     N                       N                   \
-Triple-single-quoted    '''…'''                 N                       N                   \
-Double-quoted           "…"                     Y                       N                   \
-Triple-double-quoted    """…"""                 Y                       Y                   \
-Slashy                  /…/                     Y                       Y                   \
-Dollar slashy           $/…/$                   Y                       Y                   $
-*/
-def str1 = 'qwer'
-def str2 = "qwer"
-def str3 = '''qwer
-1234'''
-def str4 = """qwer
-1234"""
-// 最后的反斜杠表示不插入 换行符 \n, 表示 当前行和下一行同属于一行
-def str4 = """qwer\                   
-1234"""
-def str5 = /qwer \/1234/              // 主要用于正则表达式，这样正则表达式中的 \ 字符，在代码里面就不用写成 \\\\ 四个反斜杠了
-def str6 = $/qwer 1234/$
+ * StringType              StringSyntax            Interpolated            Multiline           Escape character
+ * Single-quoted           '…'                     N                       N                   \
+ * Triple-single-quoted    '''…'''                 N                       N                   \
+ * Double-quoted           "…"                     Y                       N                   \
+ * Triple-double-quoted    """…"""                 Y                       Y                   \
+ * Slashy                  /…/                     Y                       Y                   \
+ * Dollar slashy           $/…/$                   Y                       Y                   $
+ * */
+def str1 = 'qwe \n r'
+def str2 = "qwe \n r"
+def str5 = /qwe \/ r/              // 主要用于正则表达式，这样正则表达式中的 \ 字符，在代码里面就不用写成 \\\\ 四个反斜杠了
+def str6 = $/qwe \n $$ r/$
 
-/******************************************************** Character ****************************************************/
-/** Groovy 中没有 char 类型，但是有 3 中方法，把 String 类型转成 Char 类型 */
-def c = 'A'
-def c = 'A' as char
-def c = (char)'A'
 
-/******************************************************** Number ****************************************************/    
+/******************************************************** Number ****************************************************/
 /** groovy 会自动将声明的数字放到对应的类型中，一般建议只使用以下两种 */
 BigInteger bi =  1_000_006g
 BigDecimal bd =  3.456g
 
 /******************************************************** Array ****************************************************/
-String[] arrStr = ['Ananas', 'Banana', 'Kiwi']
-def numArr = [1, 2, 3] as int[]
-Integer[][] matrix2 = [[1, 2], [3, 4]]
+String[] arrStr = ['Ananas', 'Banana', 'Kiwi']  // 一维数组
+def numArr = [1, 2, 3] as int[]                 // def 定义指定类型的数组
+Integer[][] matrix2 = [[1, 2], [3, 4]]          // 二维数组
 
-/******************************************************** List ****************************************************/ 
-/**   */
+
+/******************************************************** List ****************************************************/
 def list = ['a','b','c','d']
-list[0]         // == 'a'
-list[1, 3]      // == ['b','d']
-list[-1]        // 取倒数第一个
-list << 'e'     // 往 list 追加一个 'e'
-list[2..4]      // == ['b','d','e']
-def multi = [[0, 1], [2, 3]]            // 定义多维 List
-assert multi[1][0] == 2
+def multiList = [[0, 1], [2, 3]]            // 定义多维 List
+
+void showListUsage(){
+    println list[0]         // == 'a'
+    println list[1, 3]      // == ['b','d']
+    println list[-1]        // 取倒数第一个
+    println list << 'e'     // 往 list 追加一个 'e'
+    println list[2..4]      // == ['b','d','e']
+    println multiList[1][0]
+}
+
 
 /******************************************************** Map ****************************************************/
-/** todo  */
-def map = [:]       // 定义 空 map
-def map = [name:"Haku", age: 22, "string key": true]
-println map.name
-println map."string key"
-println map.["string key"]
-println "${map.name} is ${map.22} years old"
+def emptyMap = [:]       // 定义 空 map
+def ordinarayMap = [name:"Haku", age: 22, "string key": true]
+def kasei = 'string key'
+def varKeyMap = [(kasei): "Haku"]  // 以变量作为 map 的 key
+
+void showMapUsage(){
+    println map.name
+    println map."string key"
+    println map[kasei]
+    println "${map.name} is ${map.age} years old"
+}
+
+/******************************************************** Method ****************************************************/
+
+class MethodSyntax {
+    void method1(){}
+
+    void method2(Map<String, ?> map){}
+
+    // TODO 方法返回值的简化
+    def String return1(){ return 'aaaa' }
+    def String return2(){ 'aaaa' } // 省略 return ，默认返回最后一行的值
+}
+
+// TODO 方法参数为 Map 的简化
+new MethodSyntax().method2([key1: 'a', key2: 2])    // 初始
+new MethodSyntax().method2(key1: 'a', key2: 2)      // 省略中括号
+new MethodSyntax().method2 key1: 'a', key2: 2       // 省略小括号
+
+
+// TODO 函数指针
+def funcPointer = new MethodSyntax().&method1   // 相当于 C 语言的函数指针
+funcPointer()
+
+
 
 /******************************************************** Closure ****************************************************/
-/** 一个 Closure 是一个 groovy.lang.Closure 的实例  
-每一个 Closure 都有一个隐式参数 it，跟 java 里面的 this 指针一样，都是第一个参数
-Closure<String> 表示 Closure 的返回值是一个 String 实例
-*/
-Closure<String>  aClosure = { def it, String x, int y=4  ->  
+/** 一个 Closure 是一个 groovy.lang.Closure 的实例
+ 每一个 Closure 都有一个隐式参数 it，跟 java 里面的 this 指针一样，都是第一个参数
+ Closure<String> 表示 Closure 的返回值是一个 String 实例
+ */
+def closure1 = { -> println '无参 closure '}   // 定义无参闭包必须使用箭头
+def closure2 = { println "Hello $it" }  // 包含一个隐藏参数 it，指向调用者的实例
+def closure3 = { item++ }   // 包含一个参数的闭包
+def closure4 = { def it, String x, int y=4  ->   // 参数全部显式声明
     x = x.trim()
     println "x=${x};y=${y}"
     x
 }
-aClosure(3,2)           // 调用 Closure 方式 1
-aClosure.call(3,2)      // 调用 Closure 方式 2
 
-def closure2 = { println "$it"}  // 默认会有一个 it 参数
-def closure3 = { -> println '无参 closure '}   // 定义无参闭包必须使用箭头
+closure4(3,2)           // 调用 Closure 方式 1
+closure4.call(3,2)      // 调用 Closure 方式 2
 
-/** Closure delegate strategy 
-闭包中包含以下 3 个概念：
-    this:       对应闭包定义代码所在类的实例，无论当前闭包A是否定义在另一个闭包B内
-    owner:      对应闭包定义代码所在的直接外围对象，如果闭包A定义在闭包B内，闭包B又定义在类C内，那么 owner 对应的时闭包B的实例
-    delegate:   对应闭包调用时，手动设置的执行实例，默认设置为 owner
-                当一个变量名 aa 没有在闭包域内声明，那么默认使用 delegate.aa 调用
-*/
+
+
+/** Closure delegate strategy
+ * 闭包中包含以下 3 个概念：
+ *      this:       对应闭包定义代码所在类的实例，无论当前闭包A是否定义在另一个闭包B内
+ *      owner:      对应闭包定义代码所在的直接外围对象，如果闭包A定义在闭包B内，闭包B又定义在类C内，那么 owner 对应的时闭包B的实例
+ *      delegate:   对应闭包调用时，手动设置的执行实例，默认设置为 owner
+ *
+ * 闭包的委托策略：当一个变量名/方法名 aa 没有在闭包域内声明时，由委托策略来决定 aa 来自于哪里，那么默认使用 delegate.aa 调用
+ *      Closure.OWNER_FIRST: 优先使用 owner 中的属性，owner 中没有再使用 delegate 中的属性
+ *      Closure.DELEGATE_FIRST: 优先使用 delegate 中的属性，delegate 中没有再使用 owner 中的属性
+ *      Closure.OWNER_ONLY: 只使用 owner 中的属性
+ *      Closure.DELEGATE_ONLY: 只使用 delegate 中的属性
+ *      Closure.TO_SELF: 当自己实现 groovy.lang.Closure 接口时，可以自定义闭包策略
+ * */
 class ClosureDelegateStrategyDemo {
     void run(){
         def closureB = {
@@ -102,6 +135,8 @@ class ClosureDelegateStrategyDemo {
     }
 }
 
+
+
 /** 闭包 柯里化（Curry）：将多个参数的方法，拆分成一个参数的多个方法 */
 
 
@@ -109,15 +144,16 @@ class ClosureDelegateStrategyDemo {
 
 
 
-/******************************************************** Method ****************************************************/
-
 /******************************************************** Script ****************************************************/
 /** 如果一个文件中，有任何代码不在 class 类中，那么这个文件就是一个 script，
-script 会被编译成一个类，假设类名为 Suppose，这个类会继承 groovy.lang.Script 
-所有脚本都会被 复制到一个 run 方法中，
-所有文件中定义的方法，都会被设置为 Suppose 类的方法
-且 Suppose 类中有一个 main 方法，可以执行，相当于 java 中的 public static void main(String[]) 方法
-*/
+ script 会被编译成一个类，假设类名为 Suppose，这个类会继承 groovy.lang.Script
+ 所有脚本都会被 复制到一个 run 方法中，
+ 所有文件中定义的方法，都会被设置为 Suppose 类的方法
+ 且 Suppose 类中有一个 main 方法，可以执行，相当于 java 中的 public static void main(String[]) 方法
+ */
+
+
+
 
 /******************************************************** Programming Style ***************************************/
 class ProgrammingStyle {
@@ -125,7 +161,7 @@ class ProgrammingStyle {
     String name
     /** Dynamic type: 使用 def 关键字定义标识符，不需要指定标识符类型，def 定义的标识符为 Object 类型 */
     def age
-    
+
     /** Defualt method access modifier is public: 类方法默认访问修饰符为 public */
     /** Defualt method parameter: 默认方法参数 */
     def func(param1, String param2 = 'default', Class param3){
@@ -138,7 +174,7 @@ println 'Programming Style Demo Start'
 
 /** 自动生成构造函数 及 字段赋值 */
 def ps1 = new ProgrammingStyle()
-def ps2 = new ProgrammingStyle(name: "Obelix", age: 23)         
+def ps2 = new ProgrammingStyle(name: "Obelix", age: 23)
 
 /** Omitting parentheses: 顶层表达式方法调用可以省略括号，但是无参方法调用必须带括号 */
 /** public class Class: 类名可以直接当作标识符使用，作用相当于 类名.class */
@@ -146,6 +182,9 @@ ps1.func 1, 'p2', Map
 
 /** 字段可直接赋值，会自动调用 getter setter 方法 */
 ps1.name = 'kasei'
+ps1.@name = 'kasei'  // .@ 操作符会强制直接使用 name 属性，而不是调用 getter setter 方法
+
+
 
 /** with() 方法可以指定字段和方法调用的上下文 */
 ps1.with {
@@ -165,10 +204,16 @@ println ps1?.name
 assert ps1
 def result = ps?:"Defualt Value"
 
-def range1 = 1...5      // 1 到 5 包括 5
-def range2 = 1...>5     // 1 到 5 不包括 5
+def range1 = 1..5      // 1 到 5 包括 5
+def range2 = 1 ..> 5     // 1 到 5 不包括 5
 
 println 'Programming Style Demo End'
+
+
+/******************************************************** Class ***************************************/
+class GroovyClass{
+    @PackageScope String name   // 定义包访问权限
+}
 
 
 
