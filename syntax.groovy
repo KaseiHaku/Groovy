0d1523fe7060aef9a00cb236451ca258f5b27b4e
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2020-01-09 01:07:12 +0800

    Update syntax.groovy

--- syntax.groovy
+++ syntax.groovy
@@ -7,86 +7,166 @@ import java.sql.Date as SQLDate         // as 关键字用于取别名
 
 
 /******************************************************** Variable ****************************************************/
-/** 变量赋值 */
-def var0 = 'str'
-def (int var1, String var2, var3) = [1, 'q', false]
+/** TODO 变量定义 */
+String var0             // 具体类型的变量定义
+def var1                // 不定类型的变量定义
+var3                    // Script 中字段定义
 
-/******************************************************** String ****************************************************/
-/** String Summary
- * StringType              StringSyntax            Interpolated            Multiline           Escape character
- * Single-quoted           '…'                     N                       N                   \
- * Triple-single-quoted    '''…'''                 N                       N                   \
- * Double-quoted           "…"                     Y                       N                   \
- * Triple-double-quoted    """…"""                 Y                       Y                   \
- * Slashy                  /…/                     Y                       Y                   \
- * Dollar slashy           $/…/$                   Y                       Y                   $
- * */
-def str1 = 'qwe \n r'
-def str2 = "qwe \n r"
-def str5 = /qwe \/ r/              // 主要用于正则表达式，这样正则表达式中的 \ 字符，在代码里面就不用写成 \\\\ 四个反斜杠了
-def str6 = $/qwe \n $$ r/$
+/** TODO 变量赋值 */
+var4 = 123              // 一次赋值一个变量
+def (int var5, String var6, var7) = [1, 'q', false]     // 一次赋值多个变量
 
+def str0 = '123'
+def str1 = 'qwe $str0 \\ r'
+def str2 = "qwe $str0 \\ r"
+def str5 = /qwe $str0 \/ r/              // 主要用于正则表达式，这样正则表达式中的 \ 字符，在代码里面就不用写成 \\\\ 四个反斜杠了
+def str6 = $/qwe $str0 $$ r/$
+def str7 = """
+           |<root>
+           |    <level>3</level>
+           |</root>
+           """
 
-/******************************************************** Number ****************************************************/
-/** groovy 会自动将声明的数字放到对应的类型中，一般建议只使用以下两种 */
 BigInteger bi =  1_000_006g
 BigDecimal bd =  3.456g
 
-/******************************************************** Array ****************************************************/
 String[] arrStr = ['Ananas', 'Banana', 'Kiwi']  // 一维数组
 def numArr = [1, 2, 3] as int[]                 // def 定义指定类型的数组
 Integer[][] matrix2 = [[1, 2], [3, 4]]          // 二维数组
 
 
-/******************************************************** List ****************************************************/
 def list = ['a','b','c','d']
 def multiList = [[0, 1], [2, 3]]            // 定义多维 List
+list << 'e'     // 往 list 追加一个 'e'
 
-void showListUsage(){
-    println list[0]         // == 'a'
-    println list[1, 3]      // == ['b','d']
-    println list[-1]        // 取倒数第一个
-    println list << 'e'     // 往 list 追加一个 'e'
-    println list[2..4]      // == ['b','d','e']
-    println multiList[1][0]
-}
+def map1 = [:]       // 定义 空 map
+def map2 = [name:"Haku", age: 22, "string key": true]
+def map3 = [(str0): "Haku"]  // 以变量作为 map 的 key
+
+/** TODO 变量取值 */
+println var4
+println arrStr[0]
+println matrix2[0][1]
+
+println list[0]         // == 'a'
+println list[1, 3]      // == ['b','d']
+println list[-1]        // 取倒数第一个
+println list[2..4]      // == ['b','d','e']
+println multiList[1][0]
+
+println map2.name
+println map2."string key"
+println map3[str0]
+println "${map2.name} is ${map2.age} years old"
+
+/******************************************************** Operators ****************************************************/
+println 1  + 2
+println 4  - 3
+println 3  * 5
+println 3  / 2
+println 10 % 3
+println 2 ** 3
+println +3
+println -4
+println -(-1)
+def intVar = 3
+println intVar++
+println ++intVar
+println intVar--
+println --intVar
 
 
-/******************************************************** Map ****************************************************/
-def emptyMap = [:]       // 定义 空 map
-def ordinarayMap = [name:"Haku", age: 22, "string key": true]
-def kasei = 'string key'
-def varKeyMap = [(kasei): "Haku"]  // 以变量作为 map 的 key
+int a = 0b00101010
+int b = 0b00001000
+int mask = 0b11111111
+assert ((a ^ a) & mask) == 0b00000000
+assert ((a ^ b) & mask) == 0b00100010
+assert ((~a) & mask)    == 0b11010101
 
-void showMapUsage(){
-    println map.name
-    println map."string key"
-    println map[kasei]
-    println "${map.name} is ${map.age} years old"
-}
+def displayName = user.name ? user.name : 'Anonymous'
+def displayName = user.name ?: 'Anonymous'        // 上面语句的简写 变量值 为 null, void, =0, ps1.length=0 都会判定为 false
+
+
+def name = person?.name     // 不会报 NullPointerException 直接返回 null
+def name = person.@name     // .@ 操作符会强制直接使用 name 属性，而不是调用 getter setter 方法
+
+def funcPointer = ''.&toUpperCase   // C 函数指针
+
+
+def regexp = ~/foo/         // 正则表达时字面量， java.util.regex.Pattern
+def matcher = "some text to match" =~ /match/       // java.util.regex.Matcher
+def isMatch = "some text to match" ==~ /match/      // 直接返回是否匹配的结果
+
+def persons = []
+def personNames = persons*.name
+def args = [4,5,6]
+function(*args)
+def list = [1,2,3,*args,6]
+
+def m1 = [c:3, d:4]
+def map = [a:1, b:2, *:m1]
+
+def range = 1..5
+def range = 1..<5
+def range = 'a'..'g'
+
+// compareTo
+(1 <=> 1)
+
+def list = [0,1,2,3,4]
+list[0..2] = [6,6,6]
+
+
+def list = ['Grace','Rob','Emmy']
+assert ('Emmy' in list)
+
+
+def list1 = ['Groovy 1.8','Groovy 2.0','Groovy 2.3']
+def list2 = ['Groovy 1.8','Groovy 2.0','Groovy 2.3']
+assert list1 == list2
+assert !list1.is(list2)
+
+
+Integer x = 123
+String s = (String) x
+
+List<String> strings = new LinkedList<>()
+
+def mc = new MyCallable()
+assert mc.call(2) == 4
+assert mc(2) == 4
+
+<<  左移
+>>  右移
+>>>   无符号右移
+
 
 /******************************************************** Method ****************************************************/
-
+/** TODO 方法定义 */
 class MethodSyntax {
-    void method1(){}
 
-    void method2(Map<String, ?> map){}
-
-    // TODO 方法返回值的简化
+    /* TODO 方法返回值的简化 */
     def String return1(){ return 'aaaa' }
     def String return2(){ 'aaaa' } // 省略 return ，默认返回最后一行的值
-}
 
-// TODO 方法参数为 Map 的简化
-new MethodSyntax().method2([key1: 'a', key2: 2])    // 初始
-new MethodSyntax().method2(key1: 'a', key2: 2)      // 省略中括号
-new MethodSyntax().method2 key1: 'a', key2: 2       // 省略小括号
+    void method1(){}
+    void method2(Map<String, ?> map, Integer number, String str='default'){}
+
+
+}
+/** TODO 方法调用
+ * Omitting parentheses: 顶层表达式方法调用可以省略括号，但是无参方法调用必须带括号
+ * public class Class: 类名可以直接当作标识符使用，作用相当于 类名.class
+ * */
+new MethodSyntax().method2([key1: 'a', key2: 2], 3)    // 严格按照遵循参数格式及顺序
+new MethodSyntax().method2(key1: 'a', key2: 2, 3)      // 省略 Map 参数的中括号
+new MethodSyntax().method2(key1: 'a', 3, key2: 2)      // 调换参数顺序，注意:调换只有在 Map 类型的参数是方法的第一个参数时有效
+new MethodSyntax().method2 3, key1: 'a', key2: 2       // 省略方法调用的小括号
 
 
 // TODO 函数指针
 def funcPointer = new MethodSyntax().&method1   // 相当于 C 语言的函数指针
 funcPointer()
-
 
 
 /******************************************************** Closure ****************************************************/
@@ -141,6 +221,27 @@ class ClosureDelegateStrategyDemo {
 
 
 
+/******************************************************** Class ***************************************/
+class GroovyClass{
+    @PackageScope String name   // 定义包访问权限
+    def func(param1, String param2 = 'default', Class param3){
+        'return value'
+    }
+}
+
+
+/** with() 方法可以指定字段和方法调用的上下文 */
+new GroovyClass().with {
+    name = 'haku'
+    age = 22
+    func 3, 'p2', List
+}
+
+/** tap() 方法可以指定字段和方法调用的上下文，且默认返回调用者，可以使用 with(true) 替代 tap */
+def ps3 = new GroovyClass().tap {
+    name = 'miku'
+}
+
 
 
 
@@ -151,69 +252,6 @@ class ClosureDelegateStrategyDemo {
  所有文件中定义的方法，都会被设置为 Suppose 类的方法
  且 Suppose 类中有一个 main 方法，可以执行，相当于 java 中的 public static void main(String[]) 方法
  */
-
-
-
-
-/******************************************************** Programming Style ***************************************/
-class ProgrammingStyle {
-    /** No semicolons: groovy 编程不需要在语句后面加 ; 结尾 */
-    String name
-    /** Dynamic type: 使用 def 关键字定义标识符，不需要指定标识符类型，def 定义的标识符为 Object 类型 */
-    def age
-
-    /** Defualt method access modifier is public: 类方法默认访问修饰符为 public */
-    /** Defualt method parameter: 默认方法参数 */
-    def func(param1, String param2 = 'default', Class param3){
-        /** Optional return keyword: 方法体中最后一个表达式就是方法的返回值，不需要写 return 关键字 */
-        'return value'
-    }
-}
-
-println 'Programming Style Demo Start'
-
-/** 自动生成构造函数 及 字段赋值 */
-def ps1 = new ProgrammingStyle()
-def ps2 = new ProgrammingStyle(name: "Obelix", age: 23)
-
-/** Omitting parentheses: 顶层表达式方法调用可以省略括号，但是无参方法调用必须带括号 */
-/** public class Class: 类名可以直接当作标识符使用，作用相当于 类名.class */
-ps1.func 1, 'p2', Map
-
-/** 字段可直接赋值，会自动调用 getter setter 方法 */
-ps1.name = 'kasei'
-ps1.@name = 'kasei'  // .@ 操作符会强制直接使用 name 属性，而不是调用 getter setter 方法
-
-
-
-/** with() 方法可以指定字段和方法调用的上下文 */
-ps1.with {
-    name = 'haku'
-    age = 22
-    func 3, 'p2', List
-}
-
-/** tap() 方法可以指定字段和方法调用的上下文，且默认返回调用者，可以使用 with(true) 替代 tap */
-def ps3 = new ProgrammingStyle().tap {
-    name = 'miku'
-}
-/** ?. 避免 NullPointException ，如果上一级为 null 则立即返回 null */
-println ps1?.name
-
-/** ps1 为 null, void, =0, ps1.length=0 都会判定为 false */
-assert ps1
-def result = ps?:"Defualt Value"
-
-def range1 = 1..5      // 1 到 5 包括 5
-def range2 = 1 ..> 5     // 1 到 5 不包括 5
-
-println 'Programming Style Demo End'
-
-
-/******************************************************** Class ***************************************/
-class GroovyClass{
-    @PackageScope String name   // 定义包访问权限
-}
 
 
 
